5b772700b426deb52ebfce97fbc629f3fff0459c
Mark Kalinkin <mark.kalinkin.07@gmail.com>
2025-05-06 21:46:20 +0300

    Initial commit

---
+++ Model/Core/BreakablePlatform.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Core {
+    public class BreakablePlatform : Platform {
+        public override void Bounce(Player player) {
+        }
+         
+        // ...
+    }
+}
