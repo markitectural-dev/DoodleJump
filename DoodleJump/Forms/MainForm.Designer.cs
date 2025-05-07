f4cb7393b739dcc9c07879768cc72cd0e7f9a706
Mark Kalinkin <mark.kalinkin.07@gmail.com>
2025-05-07 09:08:08 +0300

    Fix forms, remove default Form1.cs

--- DoodleJump/Forms/MainForm.Designer.cs
+++ DoodleJump/Forms/MainForm.Designer.cs
@@ -1,12 +1,12 @@
-﻿namespace DoodleJump {
-    partial class Form1 {
+﻿namespace DoodleJump.Forms {
+    partial class MainForm {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing) {
@@ -19,21 +19,14 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            SuspendLayout();
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Name = "Form1";
-            Text = "Form1";
-            Load += Form1_Load;
-            ResumeLayout(false);
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "MainForm";
         }
 
         #endregion
--- DoodleJump/Program.cs
+++ DoodleJump/Program.cs
@@ -1,3 +1,5 @@
+using DoodleJump.Forms;
+
 namespace DoodleJump {
     public static class Program {
         /// <summary>
@@ -8,7 +10,7 @@ namespace DoodleJump {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new MenuForm());
         }
     }
 }
