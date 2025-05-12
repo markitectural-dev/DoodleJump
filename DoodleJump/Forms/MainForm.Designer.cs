0703668f2befca6e4928ce48ecee8fe524d61d46
Arkilleru <arkilleru.@MacBook-Pro-Arkilleru.local>
2025-05-12 19:00:00 +0300

    mainForm

--- DoodleJump/Forms/MainForm.Designer.cs
+++ DoodleJump/Forms/MainForm.Designer.cs
@@ -23,10 +23,17 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainForm";
+            this.ClientSize = new System.Drawing.Size(480, 720);
+            this.Name = "MainForm";
+            this.Text = "Doodle Jump";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
